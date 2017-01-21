dfa496d23d4f3a723d0ef0ad4afa0d3c0cb21ebd
Dennis Seidel <mail@dennisseidel.de>
2017-01-21 15:39:44 +0100

    chore: update setup

---
+++ setup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+current_dir=$PWD
+
+if [ ! -d "$current_dir/backend-service" ]; then
+  git clone https://github.com/dennisseidel/backend-service-schedulebot.git $current_dir/backend-service
+else
+  cd $current_dir/backend-service/ && git pull 
+fi
+
+if [ ! -d "$current_dir/auth-service" ]; then
+  git clone https://github.com/dennisseidel/auth-service.git $current_dir/auth-service
+else
+  cd $current_dir/auth-service/ && git pull 
+fi
+
+if [ ! -d "$current_dir/web-interface" ]; then
+  git clone https://github.com/dennisseidel/auth-service.git $current_dir/web-interface
+else
+  cd $current_dir/web-interface/ && git pull 
+fi
